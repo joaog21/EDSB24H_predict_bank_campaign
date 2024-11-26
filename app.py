3415affa63bcc74dbc01a158a3e7c9a0196a8f67
joaog21 <127127463+joaog21@users.noreply.github.com>
2024-11-26 00:05:31 +0000

    Correction

--- app.py
+++ app.py
@@ -207,13 +207,13 @@ iface = gr.Interface(
                 maximum=31,
                 step=1),
         gr.Dropdown(
-            label="Day of the Week of last contact",
-            choices=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        ),
-        gr.Dropdown(
             label="Month",
             choices=['January', 'February', 'March', 'April', 'May', 'June', 
                     'July', 'August', 'September', 'October', 'November', 'December']
+        ),
+        gr.Dropdown(
+            label="Day of the Week of last contact",
+            choices=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
         )
     ],
     outputs=gr.Textbox(label="Predicted outcome of the campaign for the client"),
